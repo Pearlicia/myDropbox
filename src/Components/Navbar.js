4a775fa4fcbbe002350e9ca9856ece01957cd549
Pearlicia <feliciaebikon@gmail.com>
2023-04-17 18:24:46 +0100

    implement logout

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,20 @@
 import React from "react"
-import { Navbar, Nav } from "react-bootstrap"
+import { Navbar, Nav, Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import { useAuthenticate } from "../Context"
+
 
 export default function NavbarComponent() {
+  const { logout } = useAuthenticate()
+  
+  const handleLogout = async () => {
+    try {
+      await logout()
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  
   return (
     <Navbar bg="primary" expand="sm" className="d-flex justify-content-between">
       <Navbar.Brand as={Link} to="/" className="text-white" fontSize="3rem">
@@ -12,9 +24,9 @@ export default function NavbarComponent() {
         <Nav.Link as={Link} to="/user" className="text-white ">
           Update Profile
         </Nav.Link>
-        <Link to="/Login"  className="p-2 text-white" >
-          Login
-        </Link>
+        <Button onClick={handleLogout} className="p-2 text-white" >
+          Logout
+        </Button>
       </Nav>
     </Navbar>
   )
